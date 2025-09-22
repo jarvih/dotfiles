14c3533aea862f1d92b0947c12be794f8e1538e7
Jahe <jahe@VK-PF4XBGM5>
2025-09-22 09:17:29 +0300

    Initial settings

---
+++ lua/config/settings.lua
@@ -0,0 +1,20 @@
+-- Numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Search
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+
+vim.opt.cursorline = true
+
+-- Tabs
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+
+-- Appearance
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.background = "dark"
