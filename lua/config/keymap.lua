a71748b7b3832325ec57f83ea0066d56f5ccc0cc
Jahe <jahe@VK-PF4XBGM5>
2025-09-22 13:23:07 +0300

    more keymaps

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -5,3 +5,12 @@ vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 -- Paste and keep clipboard content
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- Move selected lines in visual lines usin J,K
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Reload source
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
